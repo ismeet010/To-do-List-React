fc160a9f032c74cc5c0da77d8d00aaae7034da0a
ismeet010 <ismeetchhabda@gmail.com>
2023-07-25 15:58:51 +0530

    Updates

---
+++ precache-manifest.c75dbc4e7f591f0aa271d9bb5fb13c28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e168214d43b1a6b66f0",
+    "url": "/To-do-List-React/static/css/main.93e68023.chunk.css"
+  },
+  {
+    "revision": "9e168214d43b1a6b66f0",
+    "url": "/To-do-List-React/static/js/main.9e168214.chunk.js"
+  },
+  {
+    "revision": "2e0c0cd842ef5b43220a",
+    "url": "/To-do-List-React/static/js/1.2e0c0cd8.chunk.js"
+  },
+  {
+    "revision": "bcf1491c6b9f30fa084f",
+    "url": "/To-do-List-React/static/js/runtime~main.bcf1491c.js"
+  },
+  {
+    "revision": "a575d07d7a0b280e94416b7b2865e07b",
+    "url": "/To-do-List-React/index.html"
+  }
+];
